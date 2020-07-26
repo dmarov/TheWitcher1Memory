575188e444a47043ed4529708277a1aa39886bb5
Dmitry Marov <d.marov94@gmail.com>
2020-07-26 15:04:32 +0300

    fixed base module address

--- TheWitcher1Memory.cpp
+++ TheWitcher1Memory.cpp
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <TlHelp32.h>
 #include <tchar.h>
+#include <psapi.h>
 
 class Player
 {
@@ -12,50 +13,61 @@ private:
 	HWND window;
 	HANDLE handle;
 
-	std::string moduleName = "witcher.exe";
+	std::string mainModuleName = "witcher.exe";
 	std::string windowName = "The Witcher (1.4.5.1304)";
-	DWORD vitalityBaseAddr = 0x2212C2D0;
-	std::vector<DWORD> vitalityOffsets = { 0x00DC09E4, 0x29C, 0x4, 0x24, 0x0, 0x58 };
+	std::vector<DWORD_PTR> vitalityOffsets = { 0x00DC09E4, 0x29C, 0x4, 0x24, 0x0, 0x58 };
 
 private:
-	DWORD getModuleBaseAddr(TCHAR* moduleName)
+	DWORD_PTR getModuleBaseAddr(TCHAR* moduleName)
 	{
-		DWORD moduleBaseAddress = 0x0;
-		HANDLE handle = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, this->pid);
+		DWORD_PTR moduleBaseAddr = 0x0;
+		HMODULE     *moduleArray;
+		LPBYTE      moduleArrayBytes;
+		DWORD       bytesRequired;
 
-		MODULEENTRY32 entry = { 0 };
-		entry.dwSize = sizeof(MODULEENTRY32);
-
-		if (Module32First(handle, &entry))
+		if (this->handle)
 		{
-			do {
-				if (_tcscmp(entry.szModule, moduleName) == 0)
+			if (EnumProcessModules(this->handle, NULL, 0, &bytesRequired))
+			{
+				if ( bytesRequired )
 				{
-					moduleBaseAddress = (DWORD)entry.modBaseAddr;
-					break;
+					moduleArrayBytes = (LPBYTE)LocalAlloc( LPTR, bytesRequired );
+
+					if ( moduleArrayBytes )
+					{
+						unsigned int moduleCount;
+
+						moduleCount = bytesRequired / sizeof( HMODULE );
+						moduleArray = (HMODULE *)moduleArrayBytes;
+
+						if ( EnumProcessModules( this->handle, moduleArray, bytesRequired, &bytesRequired ) )
+						{
+							moduleBaseAddr = (DWORD_PTR)moduleArray[0];
+						}
+
+						LocalFree( moduleArrayBytes );
+					}
 				}
-			} while (Module32Next(handle, &entry));
+			}
 		}
 
-		CloseHandle(handle);
-
-		return moduleBaseAddress;
+		return moduleBaseAddr;
 	}
 
-	DWORD getPointerAddr(DWORD baseAddr, std::vector<DWORD> offsets)
+	DWORD getPointerAddr(DWORD_PTR baseAddr, std::vector<DWORD> offsets)
 	{
-		DWORD result = baseAddr;
-		DWORD newValue;
+		DWORD_PTR result = baseAddr;
+		DWORD_PTR newValue;
 
-		for (std::vector<DWORD>::iterator it = offsets.begin(), eit = offsets.end(); it != eit; ++it)
+		for (std::vector<DWORD_PTR>::iterator it = offsets.begin(), eit = offsets.end(); it != eit; ++it)
 		{
 			result = baseAddr + *it;
 			std::cout << std::hex << result << std::endl;
 			bool read = ReadProcessMemory(this->handle, (LPCVOID)(result), &newValue, sizeof(newValue), NULL);
 
-			//if (!read) {
-			//	throw new std::exception("unable to read memory");
-			//}
+			if (!read) {
+				throw new std::exception("unable to read memory");
+			}
 
 			baseAddr = newValue;
 		}
@@ -74,13 +86,14 @@ public:
 
 	void setVitality(float vitality)
 	{
-		DWORD moduleBaseAddr = this->getModuleBaseAddr((TCHAR*)(this->moduleName.c_str()));
+		DWORD_PTR moduleBaseAddr = this->getModuleBaseAddr((TCHAR*)(this->mainModuleName.c_str()));
+		std::cout << std::hex << moduleBaseAddr << std::endl;
 		DWORD vitalityAddr = this->getPointerAddr(moduleBaseAddr, this->vitalityOffsets);
 		bool written = WriteProcessMemory(this->handle, (LPVOID)vitalityAddr, &vitality, sizeof(vitality), 0);
 
-		//if (!written) {
-		//	throw new std::exception("unable to write to memory");
-		//}
+		if (!written) {
+			throw new std::exception("unable to write to memory");
+		}
 	}
 
 	void setMana(float mana)
@@ -95,6 +108,6 @@ int main(int argc, char **argv)
 
 	while (true) {
 		player.setVitality(10000);
-		Sleep(10000);
+		Sleep(100);
 	}
 }
